73ec5fcfac1822010d6d9f875e8e376741147e45
William MARTIN <william.martin@power-lan.com>
2017-02-19 13:53:09 +0100

    Fix bad detection of HTTP method

--- src/Request.php
+++ src/Request.php
@@ -9,5 +9,19 @@ class Request extends \OAuth2\Request
     {
         $this->initialize($request->GET, $request->POST, array(), $request->COOKIE, $request->FILES, (array) $request->mess->headers, $request->BODY, (array) $request->headers);
     }
+
+    public function server($name, $default = null)
+    {
+        switch($name) {
+            case 'REQUEST_METHOD':
+                return strtolower($this->headers('METHOD', $default));
+
+            case 'CONTENT_TYPE':
+                return $this->headers('content-type', $default);
+
+            default:
+                return parent::server($name, $default);
+        }
+    }
 }
 
